4fe7fd65747d4f6f7f082328366cf6105196ca0e
조병현 <168700800+choBottle@users.noreply.github.com>
2025-09-29 22:03:49 +0900

    Title: 입력받아 출력 2, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

--- 250929/입력받아 출력 2/input-and-output-2.java
+++ 250929/입력받아 출력 2/input-and-output-2.java
@@ -4,13 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String a = sc.next();
+        String a = sc.next();      
+        String[] arr = a.split("-"); 
 
-        String[] arr = a.split("-");
-
-        int b = Integer.parseInt(arr[0]);
-        int c = Integer.parseInt(arr[1]);
-
-        System.out.print(b + c);
+        System.out.print(arr[0] + arr[1]); 
     }
 }
