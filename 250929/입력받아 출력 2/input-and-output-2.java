da23298c5534ad6f6fa6fbe0c15c9bf89d1768c1
조병현 <168700800+choBottle@users.noreply.github.com>
2025-09-29 21:57:36 +0900

    Title: 입력받아 출력 2, Time: 114ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250929/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+
+        String[] arr = a.split("-");
+
+        int d = Integer.parseInt(arr[0]);
+        int m = Integer.parseInt(arr[1]);
+        
+        System.out.print(d + m);
+    }
+}
