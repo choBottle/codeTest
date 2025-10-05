387cb49fbee7ff9ddb39e95059c6f162fb6ddff1
조병현 <168700800+choBottle@users.noreply.github.com>
2025-10-05 18:17:39 +0900

    Title: 알파벳에 따른 평가, Time: 115ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251005/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.next();
+
+        if(n.equals("S")) {
+            System.out.print("Superior");
+        }else if(n.equals("A")) {
+            System.out.print("Excellent");
+        }else if(n.equals("B")) {
+            System.out.print("Good");
+        }else if(n.equals("C")) {
+            System.out.print("Usually");
+        }else if(n.equals("D")) {
+            System.out.print("Effort");
+        }else {
+            System.out.print("Failure");
+        }
+    }
+}
