4cab3e9fd71f6c91134e365022ab0d51abb86951
조병현 <168700800+choBottle@users.noreply.github.com>
2025-10-09 05:14:15 +0900

    Title: 굉장한 수, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 251009/굉장한 수/amazing-number.java
+++ 251009/굉장한 수/amazing-number.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        int a = sc.nextInt();
 
         if(a % 2 == 1 && a % 3 == 0) {
             System.out.print("true");
