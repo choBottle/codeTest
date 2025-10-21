ba6baa766c1b2f30fa3dfc742c42f234fd05d1d8
조병현 <168700800+choBottle@users.noreply.github.com>
2025-10-22 01:11:07 +0900

    Title: 조건에 따른 연산, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251022/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        while(true) {
+            if(n >= 1000) {
+                System.out.print(count);
+                break;
+            }
+
+            if( n % 2 == 0) {
+                n = n * 3 + 1;
+            }else {
+                n = n * 2 + 2;
+            }
+            count++;
+        }
+    }
+}
