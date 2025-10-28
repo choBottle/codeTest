8d05e9881fe19515865343cd3e7b97347f997508
조병현 <168700800+choBottle@users.noreply.github.com>
2025-10-28 18:15:46 +0900

    Title: 별표 출력하기 13, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251028/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count1 = 0;
+        int count2 = 1;
+
+        for(int i = 1; i <= n; i++) {
+            if(i%2 == 1) {
+                for(int j = 0; j < n - count1; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+                count1++;
+            }else {
+                for(int j = 0; j <count2; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+                count2++;
+            } 
+        }
+         for(int i = 0; i < n; i++) {
+            if(i%2 == 1) {
+                for(int j = 0; j < n - count1; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+                count1++;
+            }else {
+                for(int j = 0; j < count2; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+                count2++;
+            }
+        }
+    }
+}
