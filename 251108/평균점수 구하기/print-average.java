ffddf5061629e18abe8e8a36c71f5b20c0be83d7
조병현 <168700800+choBottle@users.noreply.github.com>
2025-11-08 14:19:07 +0900

    Title: 평균점수 구하기, Time: 154ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 251108/평균점수 구하기/print-average.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double sum = 0.0;
+
+        for(int i = 0; i < 8; i++) {
+            double a = sc.nextDouble();
+
+            sum += a;
+        }
+        sum /= 8;
+
+        System.out.print(String.format("%.1f",sum));
+    }
+}
