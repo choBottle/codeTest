7ceee33ffe35e91c710397d223d2f692ab2dfdc8
조병현 <168700800+choBottle@users.noreply.github.com>
2025-11-08 14:32:36 +0900

    Title: 배열 채우기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251108/배열 채우기/filling-array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        int idx = 0;
+
+        for(int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+            if(n == 0) break;
+
+            arr[idx] = n;  
+            idx++;
+        }
+
+        for(int i = idx - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
