fa67d08780a2d4d2f77f52cf7a8acc5eaf96fa9f
조병현 <168700800+choBottle@users.noreply.github.com>
2025-11-13 12:10:21 +0900

    Title: 점수대 파악하기, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251113/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {0,0,0,0,0,0,0,0,0,0};
+
+        while(true) {
+            int n = sc.nextInt();
+            if(n == 0) {
+                break;
+            }
+            n /= 10;
+            if(n == 0) {
+                continue;
+            }
+            arr[n-1]++;
+        }
+
+        for(int i = 9; i >= 0; i--) {
+            System.out.println(((i+1)*10) + " - " + arr[i]);
+        }
+    }
+}
