6eaac560a99bbe2280ad616e6365b0b4d8024567
조병현 <168700800+choBottle@users.noreply.github.com>
2025-11-14 17:11:08 +0900

    Title: 개수 세기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251114/개수 세기/count-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int m = sc.nextInt();
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            if(a == m) {
+                count++;
+            }
+        }
+
+        System.out.print(count);
+        
+    }
+}
