ec7d942574c7f43076c4aa197c6e871a2c6f8e64
조병현 <168700800+choBottle@users.noreply.github.com>
2025-11-17 18:13:15 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 131ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251117/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int max = 0;
+        int idx = 0;
+        int idx2 = 0;
+        int[] cnt = new int[5];
+
+        for(int i = 0; i < n; i++) {
+            cnt[i] = sc.nextInt();
+        }
+
+        
+
+        for(int i = 0; i < n; i++) {
+            if(max < cnt[i]) {
+                max = cnt[i];
+                idx = i+1;
+            }
+        }
+        System.out.print(idx + " ");
+        max = 0;
+        idx2 = idx;
+        idx = 0;
+
+
+        while(true) {
+            for(int i = 0; i < idx2 - 1; i++) {
+            if(max < cnt[i]) {
+                max = cnt[i];
+                idx = i+1;
+            }
+        }
+        if(idx == 0) {
+            break;
+        }
+        System.out.print(idx + " ");
+        max = 0;
+        idx2 = idx;
+        idx = 0;
+        }
+        
+    }
+}
