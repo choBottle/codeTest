77cbc9864ca602b72f1518920d37e2d7663cbc17
조병현 <168700800+choBottle@users.noreply.github.com>
2025-11-18 12:46:38 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 206ms, Memory: 13MB, Status: Passed - Codetree

--- 251118/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 251118/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -1,36 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_NUM = 1000;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        // 변수 선언:
+        int n;
+        int[] price = new int[MAX_NUM];
 
-        int count = 0;
-        int min = Integer.MAX_VALUE;
-        int max = 0;
-        int[] arr = new int[n+1];
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            price[i] = sc.nextInt();
 
+        // 배열을 앞에서부터 순회하며 사는 시점의 후보를 선택합니다
+        int maxProfit = 0;
         for(int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-            if(min > arr[i] ) {
-                min = arr[i];
-                count = i;
+            // 사는 시점의 다음 해부터 순회하며 파는 시점의 후보를 선택합니다
+            for(int j = i + 1; j < n; j++) {
+                int profit = price[j] - price[i];
+                
+                if(profit > maxProfit)
+                    maxProfit = profit;
             }
         }
 
-        for(int i = count; i <n; i++) {
-            if(max < arr[i]) {
-                max = arr[i];
-            }
-        }
-
-        if(max == 0) {
-            System.out.print(max);
-        }else {
-            System.out.print(max - min);
-        }
-
-        
+        System.out.println(maxProfit);
     }
 }
