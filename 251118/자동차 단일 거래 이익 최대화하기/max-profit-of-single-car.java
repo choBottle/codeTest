19610bf6a3dc1da601dd40a28985658c1d6854b9
조병현 <168700800+choBottle@users.noreply.github.com>
2025-11-18 12:37:19 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 175ms, Memory: 12MB, Status: Wrong Answer - Codetree

--- 251118/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 251118/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -9,7 +9,7 @@ public class Main {
         int count = 0;
         int min = Integer.MAX_VALUE;
         int max = 0;
-        int[] arr = new int[n];
+        int[] arr = new int[n+1];
 
         for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
