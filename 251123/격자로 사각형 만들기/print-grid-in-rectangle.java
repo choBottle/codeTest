a16a595a70326bdeece9b4c986ef2a933b3d0c72
조병현 <168700800+choBottle@users.noreply.github.com>
2025-11-23 18:27:17 +0900

    Title: 격자로 사각형 만들기, Time: 112ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251123/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        long[][] arr = new long[N][N];
+
+        for (int i = 0; i < N; i++) {
+            arr[0][i] = 1;
+            arr[i][0] = 1;
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                arr[i][j] = arr[i-1][j] + arr[i][j-1] + arr[i-1][j-1];
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
