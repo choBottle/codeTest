1057eb33e76cd265461f427252362eab8d2f7599
조병현 <168700800+choBottle@users.noreply.github.com>
2025-12-10 14:55:31 +0900

    Title: 부분 문자열의 개수, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251210/부분 문자열의 개수/number-of-substrings.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        String s2 = sc.next();
+
+        int count = 0;
+
+        for(int i = 0; i <= s.length()- s2.length(); i++) {
+            if(s.substring(i,i+s2.length()).equals(s2)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
