f40efff8dc032245fb83f4544c2b8a150f56c1f0
조병현 <168700800+choBottle@users.noreply.github.com>
2025-12-12 18:25:39 +0900

    Title: 두 번째를 첫 번째로, Time: 117ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251212/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        char c1 = s.charAt(0);
+        char c2 = s.charAt(1);
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == c2) {
+                System.out.print(c1);
+            }else {
+                System.out.print(s.charAt(i));
+            }
+        }
+    }
+}
