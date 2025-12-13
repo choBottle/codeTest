5f891fc439b421b6ac133fc9686525e52b62ad69
조병현 <168700800+choBottle@users.noreply.github.com>
2025-12-13 22:00:39 +0900

    Title: e 제거하기, Time: 113ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251213/e 제거하기/e-to-remove.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       String s = sc.next();
+
+       for(int i = 0; i < s.length(); i++) {
+        if(s.charAt(i) == 'e') {
+            System.out.print(s.substring(0,i) + s.substring(i+1));
+            break;
+        }
+       }
+    }
+}
