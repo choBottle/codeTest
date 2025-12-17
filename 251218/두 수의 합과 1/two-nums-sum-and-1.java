a4d31ad36c34cfe008318a609e4fecc7393066b8
조병현 <168700800+choBottle@users.noreply.github.com>
2025-12-18 04:20:44 +0900

    Title: 두 수의 합과 1, Time: 141ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251218/두 수의 합과 1/two-nums-sum-and-1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+
+        int n2 = sc.nextInt();
+
+        String s = Integer.toString(n1+n2);
+
+        int count = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '1') {
+                count++;
+            }
+        }
+
+        System.out.print(count);
+    }
+}
