f951bc72aa3d58158aa0ae55e00ec41516fc8057
조병현 <168700800+choBottle@users.noreply.github.com>
2025-12-18 04:34:32 +0900

    Title: 일치하는 문자열의 수, Time: 142ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251218/일치하는 문자열의 수/num-of-correct-string.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        String s = sc.next();
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            String a = sc.next();
+
+            if(s.equals(a)) {
+                count++;
+            }
+        }
+
+        System.out.print(count);
+    }
+}
