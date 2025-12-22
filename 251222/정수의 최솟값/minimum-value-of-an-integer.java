4296b6b6401829ee4535129c44e8929546121bfd
조병현 <168700800+choBottle@users.noreply.github.com>
2025-12-22 15:36:05 +0900

    Title: 정수의 최솟값, Time: 115ms, Memory: 10MB, Status: Passed - Codetree

--- 251222/정수의 최솟값/minimum-value-of-an-integer.java
+++ 251222/정수의 최솟값/minimum-value-of-an-integer.java
@@ -6,11 +6,11 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        if(a >= b || b <= c) {
+        if(a >= b && b <= c) {
             System.out.println(b);
-        }else if (b >= a || a <= c) {
+        }else if (b >= a && a <= c) {
             System.out.println(a);
-        }else if (b >= c || c <= a) {
+        }else if (b >= c && c <= a) {
             System.out.println(c);
         }
     }
