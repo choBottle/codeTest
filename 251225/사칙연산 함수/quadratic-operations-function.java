50d6349c72cfec5866df60048d176a15b8d6cc16
조병현 <168700800+choBottle@users.noreply.github.com>
2025-12-25 22:47:43 +0900

    Title: 사칙연산 함수, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251225/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char o = sc.next().charAt(0);
+        int c = sc.nextInt();
+        if(o == '-' ) {
+            System.out.print(a + " " + o + " " + c + " = " + (a-c));
+        }else if (o == '*') {
+            System.out.print(a + " " + o + " " + c + " = " + (a*c));
+        }else if( o == '+') {
+            System.out.print(a + " " + o + " " + c + " = " + (a+c));
+        }else if( o == '/') {
+            System.out.print(a + " " + o + " " + c + " = " + (a/c));
+        }else {
+            System.out.print("False");
+        }
+    }
+}
