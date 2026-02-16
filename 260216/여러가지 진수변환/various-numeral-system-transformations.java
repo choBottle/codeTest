8903d456841ba93173cad14e71aa7638061c4803
조병현 <168700800+choBottle@users.noreply.github.com>
2026-02-16 19:32:11 +0900

    Title: 여러가지 진수변환, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260216/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        int[] digit = new int[1000];
+
+        int cnt = 0;
+
+        while(true) {
+            if(n < b) {
+                digit[cnt++] = n;
+                break;
+            }
+
+            digit[cnt++] = n % b;
+            n /= b;
+
+        }
+
+        for(int i = cnt -1; i >=0; i--) {
+            System.out.print(digit[i]);
+        }
+    }
+}
