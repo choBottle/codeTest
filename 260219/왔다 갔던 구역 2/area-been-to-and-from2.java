f881624355306cc273d072ae044d5adb66d258cf
조병현 <168700800+choBottle@users.noreply.github.com>
2026-02-19 09:20:36 +0900

    Title: 왔다 갔던 구역 2, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 260219/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 260219/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -9,29 +9,22 @@ public class Main {
             int x = sc.nextInt();
             char dir = sc.next().charAt(0);
             if(dir == 'R') {
-                for(int j = current; j <= current+x; j++) {
+                for(int j = current; j < current+x; j++) {
                     arr[j]++;
                 }
                 current += x;
             }else {
-                for(int j = current; j >= current-x; j--) {
+                for(int j = current - 1; j >= current-x; j--) {
                     arr[j]++;
                 }
                 current -= x;
             }
         }
-        int cnt = 0;
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
-            if(cnt == 1 && arr[i] >= 2) {
+            if(arr[i] >= 2) {
                 count++;
-            }else if(arr[i] >= 2) {
-                cnt++;
-            }else if(cnt == 1 && arr[i] < 2) {
-                cnt--;
             }
-            
-            
         }
 
         System.out.println(count);
