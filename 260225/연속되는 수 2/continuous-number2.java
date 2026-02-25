6c85db86bb8a270c74b777d74b0e54f571fb8a0e
조병현 <168700800+choBottle@users.noreply.github.com>
2026-02-25 10:06:44 +0900

    Title: 연속되는 수 2, Time: 189ms, Memory: 13MB, Status: Wrong Answer - Codetree

--- 260225/연속되는 수 2/continuous-number2.java
+++ 260225/연속되는 수 2/continuous-number2.java
@@ -12,7 +12,7 @@ public class Main {
         int count = 0;
 
         for (int i = 0; i < n; i++) {
-            if(i == 0 || arr[i] != arr[i - 1]) {
+            if(i == 0 || arr[i] == arr[i - 1]) {
                 count++;
             }
         }
