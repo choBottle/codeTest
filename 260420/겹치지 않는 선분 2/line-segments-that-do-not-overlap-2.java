08f47f8ff4cc77b58744f15b4a8b5faa39abfffd
조병현 <168700800+choBottle@users.noreply.github.com>
2026-04-20 10:42:41 +0900

    Title: 겹치지 않는 선분 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260420/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] x1 = new int[n];
+        int[] x2 = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+        }
+
+        int clearLines = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean isOverlapped = false;
+
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue; 
+                if ((x1[i] < x1[j] && x2[i] > x2[j]) || (x1[i] > x1[j] && x2[i] < x2[j])) {
+                    isOverlapped = true;
+                    break; 
+                }
+            }
+
+            if (!isOverlapped) {
+                clearLines++;
+            }
+        }
+
+        System.out.println(clearLines);
+    }
+}
