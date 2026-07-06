f68b759438244ed30810181fd5e738a823b70e9e
조병현 <168700800+choBottle@users.noreply.github.com>
2026-07-06 21:04:58 +0900

    [Passed] 두 점으로 만드는 직사각형 (147ms, 10MB)

---
+++ trail2/두 점으로 만드는 직사각형/rectangle-made-of-two-points.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        int a1 = sc.nextInt();
+        int b1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int b2 = sc.nextInt();
+
+        int minX = Math.min(x1, a1);
+        int maxX = Math.max(x2, a2);
+        int minY = Math.min(y1, b1);
+        int maxY = Math.max(y2, b2);
+
+        int width = maxX - minX;
+        int height = maxY - minY;
+
+        System.out.println(width * height);
+
+        sc.close();
+    }
+}
